51a26c5b59bc881d67a8ee8246cea8e29ca46c40
Adel Suarez <adel90suarez@gmail.com>
2021-12-25 22:47:22 -0400

    Se cambia el pack por grid, se crear configuraciones para elresponsive de los frame y se separan en modulos

--- Manager.py
+++ Manager.py
@@ -1,12 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
-from GUI.Win_create import Win_create
-from GUI.Win_query import Win_query
-from GUI.Win_list import Win_list
-from GUI.Win_delete import Win_delete
-from GUI.Win_edit import Win_edit
-# from GUI.Item_table import Item_table
-from menu_bar import Menu_bar
+from setting import config
+from GUI.NavBar import Navbar
+from GUI.Table import Table
+from GUI.Notebook import Notebook
+from GUI.Notification_bar import Notification
 
 '''
 	Clase que administra todas las clases que crean ventanas y contiene el notebook que utiliza todas 
@@ -17,49 +15,29 @@ class Manager(ttk.Frame):
 	def __init__(self, master=None):
 		super().__init__(master)
 		self.master = master
-		self.master.geometry('1000x500')
-		# self.master.resizable(0,0)
 		self.create_widgets()
-		self.pack()
+		self.configure_win()
+		self.configure_self()
+		self.pack(expand=True, fill=tk.BOTH)
 
 	def create_widgets(self):
-
 		# Top bar
-		self.menu_bar = Menu_bar(self.master)
+		# self.menu_bar = Menu_bar(self.master)
 
+		# Windows
+		self.navbar = Navbar(self, bg=config.BACKGROUND_SECONDARY)
+		self.table = Table(self)
+		self.notebook =  Notebook(self)
+		self.notification_bar = Notification(self, bg='#2979ae')
 
-		# Table of articles
-		self.table_frame = ttk.Frame(self)
+	def configure_win(self):
+		"""Main window setting"""
+		self.master.geometry('1000x500')
+		self.master.title('Sistema Inventario')
 
-		self.table = ttk.Treeview(self.table_frame, columns=('name', 'price'), height=22, padding=[0,0,0,0])
-		self.table.column('#0', stretch=0, width=60, anchor=tk.CENTER)
-		self.table.heading('#0', text='ID')
-		self.table.column('name', stretch=0, width=300, anchor=tk.W)
-		self.table.heading('name', text='Producto')
-		self.table.column('price', stretch=0, width=100, anchor=tk.CENTER)
-		self.table.heading('price', text='Precio')
-
-		self.table.pack(pady=10, padx=10)
-
-		self.table_frame.pack(side=tk.LEFT)
-
-		# Options navigations
-		self.note_frame = ttk.Frame(self)
-	
-		self.notebook = ttk.Notebook(self.note_frame)
-
-		self.create = Win_create(self.notebook)
-		self.query = Win_query(self.notebook)
-		self.list = Win_list(self.notebook)
-		self.delete = Win_delete(self.notebook)
-		self.edit = Win_edit(self.notebook)
-
-		self.notebook.add(self.create, text='Carga de artículos', padding=20)
-		self.notebook.add(self.query, text='Consulta por código', padding=20)
-		self.notebook.add(self.list, text='Listado completo', padding=20)
-		self.notebook.add(self.delete, text='Borrado de artículo', padding=20)
-		self.notebook.add(self.edit, text='Modificar artículo', padding=20)
-		self.notebook.grid(row=1, column=1)
-
-		self.note_frame.pack(side=tk.RIGHT)
-
+	def configure_self(self):
+		"""Configuration of the upper menu, so that it is expandible"""
+		self.columnconfigure(0, weight=1)
+		self.rowconfigure(0, minsize=55)
+		self.rowconfigure(1, weight=1)
+		self.rowconfigure(2, minsize=20)
